67931919f5da6a68ace2c4dd311e09e18931f601
Richard Bennett <rich@n2d.co>
2015-11-21 10:37:48 -0600

    v 1.0.0

--- _working/js/core.js
+++ _working/js/core.js
@@ -4,3 +4,16 @@
 
 //GLOBAL JS HERE
 
+//EVENT HANDLERS
+$('a[href*=#]:not([href=#])').click(function() {
+if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+  var target = $(this.hash);
+  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+  if (target.length) {
+    $('html,body').animate({
+      scrollTop: target.offset().top
+    }, 1000);
+    return false;
+  }
+}
+});
